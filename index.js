78713c28a58fe300d2b7c39d58a154ecf48acdf6
Kristan Schlikow <kristian@schlikow.de>
2024-10-31 13:48:16 +0100

    Correct mistake when loading cot prompt

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {eventSource, event_types, name1, name2, saveSettingsDebounced} from '../../../../script.js';
-import {extension_settings, loadExtensionSettings } from '../../../extensions.js';
+import {extension_settings } from '../../../extensions.js';
+// Used during development
+// import {eventSource, event_types, name1, name2, saveSettingsDebounced} from '../../../../public/script.js';
+// import {extension_settings } from '../../../../public/scripts/extensions.js';
 
 const extensionName = "SillyTavern-AdditionalProxyData";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {
     cot_prompt: "{username}: [PAUSE YOUR ROLEPLAY. Answer all questions concisely, in full sentences, and continuous text.] Think about the story, and consider information you have, especially the description and setting of {character}. What do you have to consider to maintain the characters personalities? How do the characters react and what are their personality traits? What physical space are you in? How do you maintain a consistent progression?Finally: Remind yourself to not act or talk for {username}. What rules should you follow for formatting and style? Only answer the questions as instructed. Remember you are narrating a story for the user, don't include active elements for them.",
 };
@@ -31,7 +33,7 @@ eventSource.on(event_types.TEXT_COMPLETION_SETTINGS_READY, (args) =>{
     Object.assign(args, {
         'username': name1,
         'character': name2,
-        'cot_prompt': extension_settings.apd.cot_prompt,
+        'cot_prompt': extension_settings[extensionName].prompt,
     });
 });
 
@@ -42,5 +44,5 @@ jQuery(async () => {
     $('#apd_prompt').on('input', onCoTPromptInput);
     $('#apd_prompt_restore').on('click', onCoTPromptRestoreClick);
 
-    loadSettings();
+    await loadSettings();
 });
